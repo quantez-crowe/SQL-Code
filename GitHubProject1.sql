1674ca5867b2aaa53429566027f4e05ad6bd2466
Quantez Crowe, MPH <quantez.crowe@gmail.com>
2025-05-02 18:50:48 -0500

    Add files via upload

---
+++ GitHubProject1.sql
@@ -0,0 +1,11 @@
+-- TABLE
+CREATE TABLE 'customers' ('customer_id' INTEGER,'name' TEXT,'email' TEXT,'city' TEXT,'signup_date' TEXT);
+CREATE TABLE 'orders' ('order_id' INTEGER,'customer_id' INTEGER,'order_date' TEXT,'total_amount' REAL);
+CREATE TABLE 'products' ('product_id' INTEGER,'name' TEXT,'category' TEXT,'price' REAL,'stock_quantity' INTEGER);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
